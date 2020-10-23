462ecbd734f162cebc9e0d68c383d262d9b405e6
ariachuqui <ari.chuquiruna@gmail.com>
2020-10-23 11:25:36 -0300

    Probando Modificaciones

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainApp } from './MainApp';
 
+//hola como estás
+
 ReactDOM.render(
     <MainApp />,
   document.getElementById('root')
